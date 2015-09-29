aceab27d759b38e331438fb70720f2834b7c07a4
Steven Cook <steven@voltagead.com>
2015-09-29 17:22:47 -0600

    Fixed naming of artisan command in kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('reminders')
+        $schedule->command('reminders:check')
                  ->everyMinute();
     }
 }
